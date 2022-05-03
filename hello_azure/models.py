63800b646aff790f9f33661f655cd2aafee8e665
hbo2 <vanhouten@agriterra.org>
2022-05-03 20:40:00 +0200

    test

--- hello_azure/models.py
+++ hello_azure/models.py
@@ -3,14 +3,6 @@ from django.db import models
 
 # Create your models here.
 
-class Contact(models.Model): 
-   name = models.CharField(max_length=50) 
-   city = models.CharField(max_length=50) 
-   state = models.CharField(max_length=2) 
-   create_date = models.DateTimeField() 
-   phone_number = models.CharField(max_length=20) 
-   email = models.CharField(max_length=20) 
-
 class ProjectContract(models.Model):
     ProjectContractId =  models.CharField(max_length=255)
     ProjectContractName = models.CharField(max_length=255, null=TRUE)
