3eb4f6085d511d1bfbb4cf8c214b33b1a0b4f91b
hbo2 <vanhouten@agriterra.org>
2022-05-03 14:26:08 +0200

    mysql

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'quickstartproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'iati',
+        'NAME': BASE_DIR / 'iati',
         'USER': 'root',
         'PASSWORD' : 'oBQriBw*IVhK0Jvy',
         'HOST' : '10.1.9.207'
