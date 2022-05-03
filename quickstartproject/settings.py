d8c4092ce4ac928286fafbb9f784971c4c3bc638
hbo2 <vanhouten@agriterra.org>
2022-05-03 14:06:23 +0200

    test2

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'quickstartproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'db.mysql',
+        'NAME': 'mywebsite_db',
+        'USER': 'root',
+        'PASSWORD' : 'oBQriBw*IVhK0Jvy',
+        'HOST' : '10.1.9.207'
     }
 }
 
