b2e6e1e921ea40e737a5bd767df3d409bbd6120f
hbo2 <vanhouten@agriterra.org>
2022-05-03 21:30:13 +0200

    ttt

--- hello_azure/models.py
+++ hello_azure/models.py
@@ -6,20 +6,11 @@ from django.db import models
 class ProjectContract(models.Model):
     ProjectContractId =  models.CharField(max_length=255)
     ProjectContractName = models.CharField(max_length=255, null=TRUE)
-    ContractStartDate = models.CharField(max_length=255, null=TRUE)
+    ContractStartDate = models.DateTimeField()
     ContractEndDate =  models.CharField(max_length=255, null=TRUE)
     UpdatedAt = models.DateTimeField() 
     GrantApplicationId =  models.CharField(max_length=255, null=TRUE)
 
-    def __str__(self): 
-      return self.name 
-class Grant(models.Model):
-    GrantId =  models.CharField(max_length=255)
-    GrantName = models.CharField(max_length=255, null=TRUE)
-    CrantStartDate = models.CharField(max_length=255, null=TRUE)
-    GrantendDate =  models.CharField(max_length=255, null=TRUE)
-    UpdatedAt = models.DateTimeField() 
-    GContractApplicationId =  models.CharField(max_length=255, null=TRUE)
 
     def __str__(self): 
       return self.name 
