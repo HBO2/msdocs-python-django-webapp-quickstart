b2b69e418e536bfe4d9b90db2d40b23d6fb24138
hbo2 <vanhouten@agriterra.org>
2022-05-03 14:33:50 +0200

    test2

--- hello_azure/models.py
+++ hello_azure/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Person(models.Model):
+    Name = models.CharField(max_length=64)
+    Surname = models.CharField(max_length=64)
+    BirthDate = models.DateTimeField()
+    Sex = models.CharField(max_length=1)
