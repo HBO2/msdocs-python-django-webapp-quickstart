c8ab2da53c82f93d76b3dced6dd6bcfad3ed51f0
hbo2 <vanhouten@agriterra.org>
2022-05-04 11:26:29 +0200

    migration

--- hello_azure/migrations/0001_initial.py
+++ hello_azure/migrations/0001_initial.py
@@ -19,27 +19,7 @@ class Migration(migrations.Migration):
                 ('GrantendDate', models.CharField(max_length=255, null=True)),
                 ('UpdatedAt', models.DateTimeField()),
                 ('GContractApplicationId', models.CharField(max_length=55, null=True)),
-            ],
-       
-            name='ProjectContract',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('ProjectContractId', models.CharField(max_length=255)),
-                ('ProjectContractName', models.CharField(max_length=255, null=True)),
-                ('ContractStartDate', models.CharField(max_length=255, null=True)),
-                ('ContractEndDate', models.CharField(max_length=255, null=True)),
-                ('UpdatedAt', models.DateTimeField()),
-                ('GrantApplicationId', models.CharField(max_length=255, null=True)),
-            ],
-            name='Test',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('TestId', models.CharField(max_length=255)),
-                ('TestName', models.CharField(max_length=255, null=True)),
-                ('TestStartDate', models.CharField(max_length=255, null=True)),
-                ('TestendDate', models.CharField(max_length=55, null=True)),
-                ('TestApplicationId', models.CharField(max_length=55, null=True)),
-            ]
-        )
+            ],          
+        ),
     ],
     
