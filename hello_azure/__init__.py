21be674f55846516e6c28da158d9b2df6ee2de46
hbo2 <vanhouten@agriterra.org>
2022-05-04 12:16:51 +0200

    delte

---
+++ hello_azure/__init__.py
@@ -0,0 +1,1 @@
+from hello_azure.models.
--- hello_azure/models.py
+++ hello_azure/models.py
@@ -7,8 +7,27 @@ class ProjectContract(models.Model):
     ProjectContractId =  models.CharField(max_length=255)
     ProjectContractName = models.CharField(max_length=255, null=TRUE)
     ContractStartDate = models.CharField(max_length=255, null=TRUE)
-    ContractEndDate =  models.CharField(max_length=200, null=TRUE)
+    ContractEndDate =  models.CharField(max_length=255, null=TRUE)
     UpdatedAt = models.DateTimeField() 
     GrantApplicationId =  models.CharField(max_length=255, null=TRUE)
+    def __str__(self): 
+      return self.name 
+  
+class Grant(models.Model):
+    GrantId =  models.CharField(max_length=255)
+    GrantName = models.CharField(max_length=255, null=TRUE)
+    CrantStartDate = models.CharField(max_length=255, null=TRUE)
+    GrantendDate =  models.CharField(max_length=255, null=TRUE)
+    UpdatedAt = models.DateTimeField() 
+    GContractApplicationId =  models.CharField(max_length=55, null=TRUE)
+    def __str__(self): 
+      return self.name 
+class Test(models.Model):
+    TestId =  models.CharField(max_length=255)
+    TestName = models.CharField(max_length=255, null=TRUE)
+    TestStartDate = models.CharField(max_length=255, null=TRUE)
+    TestendDate =  models.CharField(max_length=55, null=TRUE)
+    TestApplicationId =  models.CharField(max_length=55, null=TRUE)
+    
     def __str__(self): 
       return self.name 
